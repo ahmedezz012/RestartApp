1fe6c0f1f869f49496cc601fc22979f6d82bb3a7
Ahmed Ezz <ahmed_ezz2021995@yahoo.com>
2023-09-10 14:16:04 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Ahmed Ezz on 10/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
